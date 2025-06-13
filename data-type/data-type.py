fb2f85bc4809ac1a4959ead0f602687d3d9b3438
syedmhamil <syedmahamil56@gmail.com>
2025-06-13 05:19:48 -0700

    compre

--- data-type/data-type.py
+++ data-type/data-type.py
@@ -1,27 +1,27 @@
-# num_int: int = 42
+num_int: int = 42
 
-# print(type(num_int)," num_int = ",num_int,)  
-
-
+print(type(num_int)," num_int = ",num_int,)  
 
 
 
-# num_float: float = int(3.14)
-# #num_float: float = .14
 
-# print(type(num_float), " num_float = ", num_float)  # <class 'float'>
+
+num_float: float = int(3.14)
+#num_float: float = .14
+
+print(type(num_float), " num_float = ", num_float)  # <class 'float'>
 
 
 
 
 # list 
 
-# my_list_1: int = [1, 2, 3, "Java", 3.14, True] #Type hinting is not enforced in python, but you should mention appropriate data type in this case 'list'
-# my_list: list = [1, 2, 3, "Python", 3.14, 3+2j]
+my_list_1: int = [1, 2, 3, "Java", 3.14, True] #Type hinting is not enforced in python, but you should mention appropriate data type in this case 'list'
+my_list: list = [1, 2, 3, "Python", 3.14, 3+2j]
 
-# print(type(my_list_1), " my_list_1 = ", my_list_1)  # <class 'list'>
-# print(type(my_list), " my_list   =  " + str(my_list))
-# print(type(3+2j)) 
+print(type(my_list_1), " my_list_1 = ", my_list_1)  # <class 'list'>
+print(type(my_list), " my_list   =  " + str(my_list))
+print(type(3+2j)) 
 
 
 
@@ -43,40 +43,40 @@
 
 # Tuples use less memory and are slightly faster than lists.
 
-# location = {
-#     (33.6844, 73.0479): "Islamabad"
-# }
+location = {
+    (33.6844, 73.0479): "Islamabad"
+}
 
-# print((location[(33.6844, 73.0479)]))
+print((location[(33.6844, 73.0479)]))
 
     
 
 
 
 
-# my_tuple: tuple = (1, 2, 3, "AI", 2.71, False, .3 , 3+2j )
-# print(my_tuple[0])
+my_tuple: tuple = (1, 2, 3, "AI", 2.71, False, .3 , 3+2j )
+print(my_tuple[0])
  
 
 
 
-#         ====================    RANGE  ================
+        # ====================    RANGE  ================
 
 
 
-# num_range: range = range(1, 10, 2) # range(start, stop, step)
-# print(type(num_range), " num_range = ", num_range.step)  # <class 'range'>
+num_range: range = range(1, 10, 2) # range(start, stop, step)
+print(type(num_range), " num_range = ", num_range.step)  # <class 'range'>
 
 
 
-## Accessing Real and Imaginary Part
+# Accessing Real and Imaginary Part
 # Python provides attributes .real and .imag to extract the real and imaginary parts of a complex number.
 
-# z = 3j + 4j +1 + 23 +45 
-# z = 3 + 4j
+z = 3j + 4j +1 + 23 +45 
+z = 3 + 4j
 
-# print("Real Part:", z.real)   # Output: 3.0
-# print("Imaginary Part:", z.imag)  # Output: 4.0
+print("Real Part:", z.real)   # Output: 3.0
+print("Imaginary Part:", z.imag)  # Output: 4.0
 
 
 
---
+++ list/comprehensive/comlist.py
@@ -0,0 +1,117 @@
+# List comprehension offers a shorter syntax when you want to create a new list based on the values of an existing list.
+
+
+
+# with out comprehensive list
+
+# fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+# newlist = []
+
+# for x in fruits:
+#   if "a" in x:
+#     newlist.append(x)
+
+# print(newlist)
+
+
+
+
+
+##################  After  COMPREHENSIVE  #################
+
+# fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+
+# newlist = [x for x in fruits if "a" in x]
+
+# print(newlist)  
+
+
+
+# ====================    Exmaple
+
+# mat = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+# res = [n for upper in mat  for n in upper ]
+
+# print(res)
+
+
+
+
+
+#Q2 #make thid from list comprehensive [[0, 1, 2, 3, 4], [0, 1, 2, 3, 4], [0, 1, 2, 3, 4], [0, 1, 2, 3, 4], [0, 1, 2, 3, 4]]
+
+
+# Nested list comprehension
+# matrix = [[n for n in range(5)] for i in range(5)]
+# print(matrix)
+
+
+
+
+# Example 2: Filtering a Nested List Using List Comprehension
+# Here, we will see how we can filter a list with and without using list comprehension.
+
+# Without Using List Comprehension
+
+# In this example, a nested loop traverses a 2D matrix, extracting odd numbers from Python list within list and appending them to the list odd_numbers. The resulting list contains all odd elements from the matrix.
+
+
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# odd_numbers = []
+# for mat in matrix:
+#     for m in mat:
+#         if m%2 != 0 :
+#            odd_numbers.append(m)
+# print(odd_numbers)
+
+#    ===========   FROM LIST COMPREHENSIVE
+
+# odd =[m for mat in matrix for m in mat if m%2 != 0]
+# print(odd)
+
+
+
+
+
+#  Example #: 
+#    you have this following promlem  yo have to above 50 mark student's list with list comprehensive
+
+
+
+data = [
+    {"name": "Ali", "marks": {"Math": 60, "English": 55}},
+    {"name": "Sara","marks": {"Math": 70, "English": 45}},
+    {"name": "Zara", "marks": {"Math": 90, "English": 95}},
+]
+
+
+# extract = [eachObj for eachObj in data for key in eachObj if eachObj["marks"]["Math"] or eachObj["marks"]["English"] >=95 ]
+
+# extract = []
+# for a in data:
+#     for key in a:
+#         if a["marks"]["Math"] >= 90 :
+#            extract.append(a)
+ 
+
+
+# import json
+
+# unique_data = list({json.dumps(d, sort_keys=True) for extract in data})
+# unique_data = [json.loads(d) for d in unique_data]
+
+
+
+
+# result  = [d["name"] for d in data if all( k >=50 for k in d["marks"].values() )] 
+result = [d["name"] for d in data if all(k >=50 for k in d["marks"].values())]
+# print(result)
+
+
+
+{"name": "Ali", "marks": {"Math": 60, "English": 55}},
+for  d in data: 
+     if all(k>50 for k in d["marks"].values()):
+          print(d["name"])
+     
