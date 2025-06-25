544927f31a9df2aa108d0d403dc0a61a913abb33
syedmhamil <syedmahamil56@gmail.com>
2025-06-25 04:56:40 -0700

    aipython

---
+++ python-ai/04_string&typecasting/string&cast.py
@@ -0,0 +1,16 @@
+# Strings in Python
+# In Python, a string is a sequence of characters enclosed in quotes (either single, double, or triple quotes). Strings are immutable, meaning they cannot be changed after they are created.
+
+# Creating Strings
+# There are several ways to create strings in Python:
+
+# Single Quotes: my_string = 'Hello, World!'
+# Double Quotes: my_string = "Hello, World!"
+# Triple Quotes: my_string = '''Hello, World!''' (can span multiple lines)
+# Raw Strings: my_string = r'Hello, World!' (treats backslashes as literal characters)
+
+
+#for multi line string use triple quotes '''any string'''
+my_string: str = '''Hello,
+World!'''
+print(my_string)
