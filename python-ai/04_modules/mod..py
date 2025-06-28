21eeaa21b0ed3c01437596d22bdc45d3a02e86a7
syedmhamil <syedmahamil56@gmail.com>
2025-06-28 06:28:44 -0700

    kwarg

--- python-ai/04_modules/mod..py
+++ python-ai/04_modules/mod..py
@@ -127,3 +127,25 @@
 
 
 
+
+
+def my_function(**student):
+  print("\nHis last name is " + student["lname"])
+
+  for key, value in student.items():
+    print(key, value)
+
+#   print(student)
+
+# my_function(fname = "Ali", lname = "Osman")
+
+# my_function(fname = "Ali", lname = "Osman", course = "Python - 101", day="Saturday", time="1400 - 1800")
+
+# my_dict = {"fname": "Arif", "lname": "Rozani", "course":"101 - 201", "day":"Saturday | Sunday", "role":"Student"}
+# my_function(**my_dict)
+# print(**my_dict)
+# my_function(my_dict) # uncomment to see TypeError
+# my_function(**my_dict) # use ** to unpack the dictionary
+
+
+
