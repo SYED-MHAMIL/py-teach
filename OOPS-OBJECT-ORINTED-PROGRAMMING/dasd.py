4e3493097dcf0fe0d84e7941d69528fbd01582c9
syedmhamil <syedmahamil56@gmail.com>
2025-08-01 18:32:56 +0500

    __str__ , __add__

--- OOPS-OBJECT-ORINTED-PROGRAMMING/dasd.py
+++ OOPS-OBJECT-ORINTED-PROGRAMMING/dasd.py
@@ -1,12 +1,76 @@
+# class Point:
+#     def __init__(self,x,y):
+#         self.x =x
+#         self.y=y
+#     def __add__(self,other):
+#        add =Point(self.x+other.x,self.y+other.y)
+#        return add
+#     def __str__(self):
+#         return(f"point({self.x},{self.y})")
+        
+#     @staticmethod
+#     def d():
+#         return "Static"
+
+# p1=Point(1,3)
+# p2 =Point(2,4)
+# newOp = p1+p2
+# print(newOp)
+
+
+
+
+
+
 class Point:
     def __init__(self,x,y):
         self.x =x
         self.y=y
     def __add__(self,other):
-       return Point(self.x+other.x,self.y+other.y)
+       return self.x+other.x,self.y+other.y # wrong
+    #  but __str__ does not work because this want to a oject itself
     def __str__(self):
-        return("hello syed mohamil")
+        return(f"point({self.x},{self.y})")
+        
+    @staticmethod
+    def d():
+        return "Static"
+
 p1=Point(1,3)
 p2 =Point(2,4)
 newOp = p1+p2
-print(newOp)   
+# print(newOp)
+
+
+
+
+
+
+# **************************************************************************************
+# **************************************************************************************
+
+# **2.__str__ vs __repr__ - Toy Labels**
+#   - __str__: Friendly display (for customers)
+#   - __repr__: Exact specs (for warehouse staff)
+
+# **************************************************************************************
+# **************************************************************************************
+
+
+class Puzzle:
+    def __init__(self,pieces):
+        self.pieces = pieces
+    def __str__(self):
+            return f"Puzzle with {self.pieces} fun pieces!"
+
+    def __repr__(self):
+        return f'pieces{self.pieces}'
+        
+
+p1 = Puzzle("100")
+# print(str(p1))
+
+
+
+
+
