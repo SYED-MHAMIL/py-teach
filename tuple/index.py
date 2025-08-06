4594322562af0c7b7dba2f4082f119d4654d65e0
syedmhamil <syedmahamil56@gmail.com>
2025-08-06 17:52:40 +0500

    add

--- tuple/index.py
+++ tuple/index.py
@@ -17,6 +17,17 @@
 
 
 
+# Why Use Tuples?
+
+# Immutable: Since tuples cannot be changed, they are safer to use when you want to ensure that the data remains constant.
+# Hashable: Tuples can be used as keys in dictionaries because they are immutable.
+# Performance: Tuples are generally faster than lists because of their immutability.
+# When to Use Tuples vs Lists
+# Use tuples when you want to store data that should not change (e.g., coordinates, constants).
+# Use lists when you need a collection that can be modified (e.g., adding or removing elements).
+
+
+
 
 
         
