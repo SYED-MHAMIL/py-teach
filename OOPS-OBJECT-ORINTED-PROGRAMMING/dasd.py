061ca849bcf211681cd8bea044459eb0f711b40c
syedmhamil <syedmahamil56@gmail.com>
2025-08-13 20:01:12 +0500

    oops

--- OOPS-OBJECT-ORINTED-PROGRAMMING/dasd.py
+++ OOPS-OBJECT-ORINTED-PROGRAMMING/dasd.py
@@ -132,8 +132,8 @@ c = Car("Toy")
 car = c.create("Toyata")
 truck = Truck.create('Ford')
 
-print(type(car),car.brand)
-print(type(truck),truck.brand)
+# print(type(car),car.brand)
+# print(type(truck),truck.brand)
 
 
 
@@ -157,5 +157,94 @@ c = Car("Toy")
 car = c.create("Toyata")
 truck = Truck.create('Ford')
 
-print(type(car),car.brand)
-print(type(truck),truck.brand)
+# print(type(car),car.brand)
+# print(type(truck),truck.brand)
+
+
+
+
+
+
+
+# +++++++++++++++++++++++++++++++++++++++++++
+#              Abstraction
+# ++++++++++++++++++++++++++++
+
+
+# Define a Car class with abstraction and idle stop engine feature
+class Car:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+        self._engine_status = False
+        self._idling_time = 0
+        self._traffic_signal = None
+
+    # Expose a drive method without revealing internal mechanics
+    def drive(self):
+        if self._engine_status and self._traffic_signal == "green":
+            print(f"You are driving a {self.brand} {self.model}.")
+        elif self._traffic_signal == "red":
+            print(f"You stopped at the red light in a {self.brand} {self.model}.")
+            self._idle_stop_engine()
+        else:
+            self._start_engine()
+            print(f"You are driving a {self.brand} {self.model}.")
+
+    # Expose a stop method to stop the car
+    def stop(self):
+        if self._engine_status:
+            print(f"You stopped the {self.brand} {self.model}.")
+            self._idle_stop_engine()
+        else:
+            print(f"The {self.brand} {self.model} is already stopped.")
+
+    # Expose a set_traffic_signal method to set the traffic signal
+    def set_traffic_signal(self, signal):
+        self._traffic_signal = signal
+        if signal == "red":
+            print(f"Traffic signal turned red. You stopped in a {self.brand} {self.model}.")
+            self._idle_stop_engine()
+        elif signal == "green":
+            print(f"Traffic signal turned green. You can drive your {self.brand} {self.model}.")
+            if not self._engine_status:
+                self._start_engine()
+
+    # Internal method to start the engine (hidden from user)
+    def _start_engine(self):
+        self._engine_status = True
+        self._idling_time = 0
+        print("Engine started.")
+
+    # Internal method to stop the engine if idling for 5 seconds (hidden from user)
+    def _idle_stop_engine(self):
+        import time
+        print("Engine is idling...")
+        for i in range(5):
+            print(f"Idling time: {i+1} seconds")
+            time.sleep(1)
+            self._idling_time += 1
+        if self._idling_time >= 5:
+            self._engine_status = False
+            print("Engine stopped (idle stop) to save fule.")
+
+
+class ShopingMall:
+    def basement_parking():
+        car = Car('Toyata','Camri')
+        car._start_engine()
+    basement_parking()
+
+   
+class BMW(Car):
+    def __init__(self, brand, model):
+        super().__init__(brand, model)
+
+    def start_engine(self):
+        self._start_engine()
+    def _start_engine(self):
+        print("BMW engine is started")
+
+bmw: BMW = BMW("BMW", "i5");
+bmw.start_engine();
+
