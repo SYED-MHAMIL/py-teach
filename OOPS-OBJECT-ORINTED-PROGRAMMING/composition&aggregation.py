f047812003f70287616d84557fcd23f498474f94
syedmhamil <syedmahamil56@gmail.com>
2025-09-22 20:11:23 +0500

    compaggregation

--- OOPS-OBJECT-ORINTED-PROGRAMMING/composition&aggregation.py
+++ OOPS-OBJECT-ORINTED-PROGRAMMING/composition&aggregation.py
@@ -15,9 +15,55 @@ class Car:
         self.engine = Engine()
 
 car1 =Car()
-print(car1.engine.run_engine())
+# print(car1.engine.run_engine())
          
 
+
+# **************************************************
+#            Example #2               
+# **************************************************
+
+class Pages:
+    def __init__(self,number):
+        self.number = number 
+        print(f"Page {self.number} created")
+    
+    def __del__(self):
+                print(f"Page {self.number} destroyed")
+    
+    
+class Book:
+     def __init__(self,nums_pages):
+        print(f"Book created")
+        self.pages = [Pages(i) for  i in range(1,nums_pages+1)] 
+     
+     def __del__(self):
+                print(f"****************  BOOK  destroyed ***********")
+    
+book1 = Book(3)
+del book1
+# print(book1.pages)
+
+
+
+# ***   what is the difference b/w Inheritance and Composition and when to use Inheritance and when to use Composition?
+# Inheritance is uesd where the class wants to nature of parents class and then modify  OR  extend the functionality of it .Inheritance will extend the functionality with extra features allows overriding of methods, but in the case of Composition, we can only use that class we can not modify or extend the functionality of it. It will not provide extra features. Thus, when one needs to use the class as it without any modification, the composition is recommended and when one needs to change the behavior of the method in another class, then inheritance is recommended.  
+
+class Parent:
+    def m1(self):
+        print("Parent class method called")
+
+
+class Child(Parent):
+    def __init__(self):
+        print("child class object created")
+    
+    def m2(self):
+        print("Child class method called")
+
+# c1 = Child()
+# c1.m1()
+# c1.m2()
 
 
 
@@ -52,8 +98,4 @@ class Department:
 depart = Department("AI ENGINEER")
 fast  = University("fast")
 fast.addDeparment(depart)
-print(fast.departments)
-
-
-
-
+# print(fast.departments)
